b1b559fc0241e21e42e7e1e2491c97317904bf14
zy <584450903@qq.com>
2018-12-27 18:01:32 +0800

    Signed-off-by: zy <584450903@qq.com>

--- ui/BaseComponent.js
+++ ui/BaseComponent.js
@@ -1,5 +1,4 @@
 import React, {Component, PureComponent} from "react";
-import PropTypes from 'prop-types';
 
 import {StyleSheetAdapt} from "./../api/StyleSheetAdapt";
 import {Tools} from "./../api/Tools";
